b023776d3670f6ee775145c9f9919a27f8e16ee8
Dante Razo <dante@razonet.org>
2023-02-16 23:53:08 -0600

    Add new wrapper scripts

---
+++ hardware/bad_blocks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+### wrapper script to detect bad blocks on given storage device
+
+## bash colors
+red="\033[0;31m"
+green="\033[0;32m"
+orange="\033[0;33m"
+yellow="\033[1;33m"
+nocolor="\033[0m"
+
+## sudo timeout trick
+while true; do sudo -nv; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+## input / arguments
+drive_arg=${1:-"noarg"}
+
+if [ $drive_arg == "noarg" ]; then
+    #if given no arguments, ask user for input
+    lsblk -e7
+    echo -en "\nWhich ${orange}storage device${nocolor} would you like to benchmark? "
+    read drive
+
+    sudo badblocks -b 4096 -v /dev/$drive
+else
+    # else, use given argument
+    sudo badblocks -b 4096 -v /dev/$drive_arg
+fi
