30cd2f802ffceb81bcfb5d05b1bb9cacb75b7e91
Dante Razo <dante@razonet.org>
2023-03-05 03:29:11 -0600

    Add comments

--- installation/install_mergerfs.sh
+++ installation/install_mergerfs.sh
@@ -1,8 +1,17 @@
+# install prereqs
 sudo apt install g++ pkg-config git git-buildpackage pandoc debhelper libfuse-dev libattr1-dev -y
+
+# clone code
 git clone https://github.com/trapexit/mergerfs.git 
 cd mergerfs
+
+# build
 make clean
 make deb
 cd ..
+
+# install
 sudo dpkg -i mergerfs*_amd64.deb
+
+# clean up
 rm mergerfs*_amd64.deb mergerfs*_amd64.changes mergerfs*.dsc mergerfs*.tar.gz
