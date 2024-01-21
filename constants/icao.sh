7d182421bcde355c5081752245c0454e0d831bd8
Dante Razo <dante@razonet.org>
2024-01-21 20:57:48 +0000

    Use constants file for ICAO

---
+++ constants/icao.sh
@@ -0,0 +1,2 @@
+btownICAO="KBMG"
+mplsICAO="KMSP"
--- fun/local_weather.sh
+++ fun/local_weather.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # Prints local weather information for the MOTD
 
-# bash formatting
+# common constants
 source /home/dante/scripts/constants/bash_formatting.sh
+source /home/dante/scripts/constants/icao.sh
 
 # defaults
 #city="Bloomington, IN"
 #ICAO="KBMG"
 city="Minneapolis, MN"
-ICAO="KMSP"
+ICAO=$mplsICAO
 
 # weather report
 echo "Current conditions in ${city} (${ICAO}):"
