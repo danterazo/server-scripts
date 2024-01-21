0ff2bbe86d4e5c4451a6875b32251bf6ea2f9305
Dante Razo <dante@razonet.org>
2024-01-21 03:03:14 -0600

    Add IP geolocation script

---
+++ security/geoip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+### script for IP geolocation
+
+## common
+source /home/dante/scripts/constants/bash_formatting.sh
+source /home/dante/scripts/constants/sudo_timeout.sh
+
+## input / arguments
+## f = full
+## r = region
+geoip_arg=${1:-"f"}
+
+# curl --silent "https://ipinfo.io/"
+
+# formatting
+echo -en "${yellow}"
+
+# print results
+if [ $geoip_arg == "f" ]; then
+    # if given no arguments, print full report
+    curl --silent "https://ipinfo.io/"
+elif [ $geoip_arg == "f" ]; then
+    curl --silent "https://ipinfo.io/region"
+else
+    # else, use region
+    curl --silent "https://ipinfo.io/region"
+fi
+
+# end formatting
+echo -e "${nocolor}"
