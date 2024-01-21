f71ce3efec0c225d60c038fa52b1bef11a337500
Dante Razo <dante@razonet.org>
2024-01-21 21:00:24 +0000

    Add Borg backup script (WIP)

---
+++ backups/borg_bkp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+### script to initiate borg backups
+
+## input / arguments
+mode=${1:-"c"}
+
+## common
+source /home/dante/scripts/constants/bash_formatting.sh
+source /home/dante/scripts/constants/sudo_timeout.sh
+
+# local variables
+borg_bkp_name=sgsys_$(date +%Y-%m-%d_%H-%M-%S)
+compression_lvl=9   # go big or go home
+
+## funny goat go baaaaaaa
+goatthink -b "Kicking off Borg system backup job."
+
+# kick off borg backup of entire system
+borg create --compression lz4,$compression_lvl $BORGPATH::$borg_bkp_name /
+
+if [[ $mode == "c" ]]; then
+    goatthink -b "Compacting Borg Repo: $BORGREPO."
+    borg compact $BORGPATH
+fi
