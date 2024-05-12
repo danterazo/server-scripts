994ba7840c751cac10cf91c9fc258404f260131f
Dante Razo <dante@razonet.org>
2024-05-12 01:11:31 -0500

    Add gravity-sync update

--- specific/smaskifa/init/smaskifa-startup.sh
+++ specific/smaskifa/init/smaskifa-startup.sh
@@ -4,3 +4,6 @@
 # update Plexamp Headless
 echo -e 'Updating Plexamp Headless...'
 source ~/plexamp/upgrade.sh > /dev/null 2>&1 || true
+
+# update gravity-sync (for PiHole)
+gravity-sync update
--- specific/staralfur/init/staralfur-startup.sh
+++ specific/staralfur/init/staralfur-startup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 ## sourced when user runs startup.sh or "startup" alias. contains stuff specific to staralfur
+
+# update gravity-sync (for PiHole)
+gravity-sync update
