bd0dab62be2775fb415848f8332a8ca547c62bbc
Dante Razo <dante@razonet.org>
2024-05-12 05:30:07 -0500

    Clean slate

--- specific/ovedur/init/ovedur-startup.sh
+++ specific/ovedur/init/ovedur-startup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 ## sourced when user runs startup.sh or "startup" alias. contains stuff specific to ovedur (ubuntu, wsl2)
-
-# restart WireGuard VPN connection
-sudo wg-quick down wg0 && sudo wg-quick up wg0
