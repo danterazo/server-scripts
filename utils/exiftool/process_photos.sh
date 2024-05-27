8044365f2b8249466f2a608fbcd487e1f9c8dd14
Dante Razo <dante@razonet.org>
2024-05-27 01:01:50 -0400

    Attribution

--- utils/exiftool/process_photos.sh
+++ utils/exiftool/process_photos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## description: preserve original filename as EXIF XMP data for photos
+## description: preserve original filename as EXIF XMP data for photos, and apply additional attributes
 ## scope: current working directory
 ## usage: use after importing (and renaming) photos from camera to Windows using "EOS Utility"
 
@@ -27,11 +27,12 @@ args=(
     '-Title<${FileName;s/\..*//}'
     '-PreservedFileName<${FileName;s/\..*//}'
 
-    # automatically fill "creator" fields across EXIF, XMP-DC, XMP-CC, and XMP-Photoshop namespaces
-    '-Artist=Dante Razo'
-    '-Creator=Dante Razo'
-    '-CaptionWriter=Dante Razo'
-    '-Credit=Dante Razo'
+    # automatically fill attribution fields across EXIF, XMP-DC, XMP-CC, and XMP-Photoshop namespaces
+    -Artist=Dante Razo
+    -Copyright=© Dante Razo
+    -Creator=Dante Razo
+    -CaptionWriter=Dante Razo
+    -Credit=Dante Razo
 )
 
 # execute in working directory + format output
