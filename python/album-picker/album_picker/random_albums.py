c4b1d54bf09be293274daefd142a03e4a28a834c
Dante Razo <dante@razonet.org>
2024-05-31 04:20:29 -0400

    Create Python module for random album picker project

--- python/album-picker/album_picker/random_albums.py
+++ python/album-picker/album_picker/random_albums.py
@@ -11,13 +11,21 @@ inventory = user.inventory
 # list albums
 print(inventory.count)
 
-# TODO: work
 # Format: Artist - Album (Year)
 
 # print results
-cds = ["The Ocean Blue - Beneath the Rhythm and Sound (1991)", "The Ocean Blue - Cerulean (1XXX)", "CD3"]
-records = ["TOB - Record1", "TOB - Record2"]
-cassettes = ["TOB - Cassette"]
+cds: list[str] = ["The Ocean Blue - Beneath the Rhythm and Sound (1991)", "The Ocean Blue - Cerulean (1XXX)", "CD3"]
+records: list[str] = ["TOB - Record1", 
+                      "TOB - Record2"]
+cassettes: list[str] = ["TOB - Cassette"]
+
+@dataclass
+def album():
+    artist: str
+    format: str
+
+    def __post_init__(self):
+        raise NotImplementedError
 
 # # print CDs
 # print(f"CDs:")
@@ -31,7 +39,8 @@ cassettes = ["TOB - Cassette"]
 # print(f"\nCassettes:\n")
 # print(*cassettes, sep="\n")
 
-df = pd.DataFrame(list(zip(cds, records, cassettes)), columns=["CDs", "Records", "Vinyl"])
+df = pd.DataFrame(list(zip(cds, records, cassettes)), 
+                  columns=["CDs", "Records", "Vinyl"])
 
 from IPython.display import display
 from tabulate import tabulate
@@ -46,3 +55,10 @@ display(df)
 
 # for row in table_data:
 #     print("{: >40} {: >40} {: >40}".format(*row))
+
+# TODO: add custom TODO tags
+# TODO: remove below
+# TODO: args for package types
+# IDEA: make python into own package with venv and management
+test: str = "teststr"
+print(f"test (type {type(test)}: {test}")
