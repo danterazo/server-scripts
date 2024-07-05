9788ea83e8c193cd9e0b7adf635cd3d47c7e94da
Dante Razo <dante@razonet.org>
2024-07-04 20:23:25 -0500

    Update phrasing in description

--- config/policy/pihole/startup.sh
+++ config/policy/pihole/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## sourced when user runs startup.sh or "startup" alias
-## contains code specific to pihole systems
+## contains code specific to systems running pihole
 
 # update gravity-sync (for PiHole)
 gravity-sync update
