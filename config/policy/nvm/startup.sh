3cd29b0a713db036f60db85d245d29a9ea7d2661
Dante Razo <dante@razonet.org>
2024-07-04 20:23:33 -0500

    Add new policy files

---
+++ config/policy/nvm/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+## sourced in non-login interactive shells, such as terminals
+## contains code specific to systems with NVM
---
+++ config/policy/pihole/bashrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## sourced in non-login interactive shells, such as terminals
+## contains code specific to systems running pihole
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
