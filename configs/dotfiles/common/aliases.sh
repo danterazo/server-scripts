f6a36a56890265aa46ad7c75f34e656d368cf21b
Dante Razo <dante@razonet.org>
2024-08-22 15:26:30 -0500

    Add Docker aliases

--- configs/dotfiles/common/aliases.sh
+++ configs/dotfiles/common/aliases.sh
@@ -4,3 +4,10 @@
 alias ufwl="sudo cat /var/log/ufw.log" # ufw logs
 alias occ="sudo docker exec --user www-data -it nextcloud-aio-nextcloud php occ" # nextcloud AIO occ
 alias gcm="git commit -m"
+
+# docker general
+alias dcd="docker compose down"
+alias dcu="docker compose up -d"
+
+# docker services
+alias occ="sudo docker exec --user www-data -it nextcloud-aio-nextcloud php occ" # nextcloud AIO container OCC
