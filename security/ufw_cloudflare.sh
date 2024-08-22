774ae19e8e7f52e275e7d1b750fe50262ac4fd7e
Dante Razo <dante@razonet.org>
2024-08-22 17:16:01 -0500

    Capitalize variable name

--- security/ufw_cloudflare.sh
+++ security/ufw_cloudflare.sh
@@ -2,10 +2,10 @@
 ## source: https://github.com/Paul-Reed/cloudflare-ufw/blob/master/cloudflare-ufw.sh
 
 # allow cloudflare IPs
-for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80,443 comment "Cloudflare IP"; done
+for CLOUDFLARE_IP in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from ${CLOUDFLARE_IP} to any port 80,443 comment "Cloudflare IP"; done
 
 # delete cloudflare IP exceptions
-# for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw delete allow proto tcp from $cfip to any port 80,443 comment "Cloudflare IP"; done
+# for CLOUDFLARE_IP in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw delete allow proto tcp from ${CLOUDFLARE_IP} to any port 80,443 comment "Cloudflare IP"; done
 
 # reload ufw
 ufw reload #> /dev/null
