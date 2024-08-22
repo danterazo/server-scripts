43ce597c2e82c5589bca65f8120e32b2e104e769
Dante Razo <dante@razonet.org>
2024-08-21 23:14:45 -0500

    Update Borg backup scope

--- backups/borg_backup.sh
+++ backups/borg_backup.sh
@@ -35,7 +35,7 @@ borg create \
     --exclude '/self-hosted/nextcloud/data' \
     \
     ::$ARCHIVENAME \
-    /etc /root /var /opt /srv /usr/local/bin /usr/local/sbin /self-hosted /ansible
+    /home /etc /root /var /opt /srv /usr/local /self-hosted /ansible
 
 backup_exit=$?
 
