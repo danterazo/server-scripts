b451dc7810d6bad7c604d5df4ecca1472404a560
Dante Razo <dante@razonet.org>
2024-08-24 20:29:54 -0500

    Update RAMDisk alias

--- configs/dotfiles/common/aliases.sh
+++ configs/dotfiles/common/aliases.sh
@@ -7,7 +7,7 @@ alias myip="curl https://ipinfo.io/ip"
 alias sn="sudo nano"
 alias sc="sudo cat"
 alias delete-empty="find . -type d -empty -print -delete"
-alias ramdisk="cd /tmp/ramdisk"
+alias ramdisk="cd /tmp/ramdisk && ls -al"
 
 # git
 alias gcm="git commit -m"
