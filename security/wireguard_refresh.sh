99773b06b23346376e2406741cdd6d10aa6fdd7e
Dante Razo <dante@razonet.org>
2024-08-29 18:15:14 -0500

    Capitalize variables

--- security/wireguard_refresh.sh
+++ security/wireguard_refresh.sh
@@ -5,39 +5,39 @@
 sudo-timeout
 
 ## constants
-wg_profile_src_dir=/home/dante/.config/wireguard/
-wg_profile_dst_dir=/etc/wireguard/
-wg_service_name=wg0
+CONFIG_SRC=/home/dante/.config/wireguard/
+CONFIG_DST=/etc/wireguard/
+WG_NAME=wg0
 
 ## input / arguments
-new_wg_profile=${1:-"noarg"}
+NEW_WG_PROFILE=${1:-"noarg"}
 
 ## defaults
 default_wg_profile=us-co-10
 
-if [ $new_wg_profile == "noarg" ]; then
+if [ ${NEW_WG_PROFILE} == "noarg" ]; then
     # if given no arguments, ask user for input
     echo
-    sudo ls $wg_profile_src_dir | sed s:.conf:: | sort -u
+    sudo ls $CONFIG_SRC | sed s:.conf:: | sort -u
     echo -en "\nWhich ${ORANGE}Wireguard Profile${NOCOLOR} would you like to use? "
-    read new_wg_profile
+    read NEW_WG_PROFILE
 else
     # else, use given argument
-    new_wg_profile=${1}
+    NEW_WG_PROFILE=${1}
 fi
 
 # bring down VPN and deluge service
 echo -e "Stopping Deluge and Wireguard..."
 sudo service deluged stop
-sudo wg-quick down ${wg_service_name}
+sudo wg-quick down ${WG_NAME}
 echo
 
 # copy
-echo -e "Applying ${ORANGE}${new_wg_profile}${NOCOLOR} profile"
-sudo cp ${wg_profile_src_dir}${new_wg_profile}.conf ${wg_profile_dst_dir}${wg_service_name}.conf
+echo -e "Applying ${ORANGE}${NEW_WG_PROFILE}${NOCOLOR} profile"
+sudo cp ${CONFIG_SRC}${NEW_WG_PROFILE}.conf ${CONFIG_DST}${WG_NAME}.conf
 echo
 
 # restore VPN and deluge
 echo -e "Starting Deluge and Wireguard..."
 sudo service deluged start
-sudo wg-quick up ${wg_service_name}
+sudo wg-quick up ${WG_NAME}
