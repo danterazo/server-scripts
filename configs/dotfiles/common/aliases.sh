c9868c6a40929be7cd3ebc664d4372a21ecee758
Dante Razo <dante@razonet.org>
2024-09-02 05:29:10 -0500

    Add fail2ban alias

--- configs/dotfiles/common/aliases.sh
+++ configs/dotfiles/common/aliases.sh
@@ -17,4 +17,5 @@ alias dcd="docker compose down"
 alias dcu="docker compose up -d"
 
 # docker services
+alias fail2ban="docker exec -t fail2ban fail2ban-client"
 alias occ="sudo docker exec --user www-data -it nextcloud-aio-nextcloud php occ" # nextcloud AIO container OCC
